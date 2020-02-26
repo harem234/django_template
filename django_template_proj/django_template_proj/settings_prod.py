7501b6282816493d5089788ed65a7e526fd80913
mahdi hamze <ssdhdd85@gmail.com>
2020-02-26 14:45:24 +0330

    heroku.yml

--- django_template_proj/django_template_proj/settings_prod.py
+++ django_template_proj/django_template_proj/settings_prod.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 jj = os.path.join
@@ -122,6 +122,10 @@ DATABASES = {
         }
     }
 }
+
+# import dj_database_url
+#
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -273,3 +277,6 @@ LOGGING = {
         },
     },
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
